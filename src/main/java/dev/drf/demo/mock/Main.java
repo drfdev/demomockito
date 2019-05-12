2373f75405ab5bbb8669fce19a69019edfa38931
drfdev <drafff@gmail.com>
2019-05-12 19:24:25 +0300

    Sources

---
+++ src/main/java/dev/drf/demo/mock/Main.java
@@ -0,0 +1,11 @@
+package dev.drf.demo.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Empty Main-class");
+    }
+}
